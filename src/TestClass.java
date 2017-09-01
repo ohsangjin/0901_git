ab137de4d350cc02799147c12d92fd4d207328de
3-14 <3-14@192.168.0.67>
2017-09-01 11:23:05 +0900

    재훈

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("삭제 하세요");
+		System.out.println("삭제 하세요!!!");
 	}
 
 }
